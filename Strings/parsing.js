9d6fae11d177a39fee27577915277539d139850a
Jalen Battle <jalenbattle78@gmail.com>
2024-10-15 13:28:30 -0400

    Walkthrough and parsing

---
+++ Strings/parsing.js
@@ -0,0 +1,16 @@
+
+// let name1 = "Brenda Kaye";
+
+
+function parseAndDisplayName(name1) {
+    let first = name1.substring(0, 6)
+    let last = name1.substring(7, 11)
+
+    console.log(name1)
+    console.log(first)
+    console.log(last)
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
---
+++ Strings/string_index.js
@@ -0,0 +1,14 @@
+text = "Now is the time for all good people..."
+index = text.indexOf(" ")
+
+console.log(index)
+console.log(text.length)
+
+firstDot = text.indexOf(".")
+console.log(firstDot)
+
+nPosition = text.indexOf("N")
+console.log(nPosition)
+
+zPosition = text.indexOf("z")
+console.log(zPosition) // -1 not found
