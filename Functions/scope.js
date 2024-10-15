41b54e9871f9b31982dcaab8f94467fb9deed89b
Jalen Battle <jalenbattle78@gmail.com>
2024-10-15 11:47:43 -0400

    added some comments to the walkthrough

--- Functions/scope.js
+++ Functions/scope.js
@@ -1,8 +1,10 @@
 eilish = "billie" // Global Scope Variable
 
 function classRoom() { // Start Block
+    // Inner Scope
     let firstName = "Jalen"; // Block (Created and Exists Here)
     // If I remove "let" / "var" / "const" the variable will become global
+
     console.log(`${firstName} is me!`);
     console.log(`${firstName} likes to listen to ${eilish}`) 
 } // End Block
@@ -11,5 +13,10 @@ console.log(`${eilish} has infectious beats.`)
 
 classRoom();
 
+// firstName does not exist in the global scope
 console.log(`${firstName} is our teacher`) // firstName does not exist in the global scope
 // firstName is not defined resluting in an error
+
+// Global
+// Local
+// Function / Block Scope {{nesting like russian doll}}
