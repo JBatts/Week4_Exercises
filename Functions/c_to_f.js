04343a5af4978533d531b423440b2a2fd9d544b4
Jalen Battle <jalenbattle78@gmail.com>
2024-10-15 10:30:11 -0400

    Made the conversion for Celsius

---
+++ Functions/c_to_f.js
@@ -0,0 +1,9 @@
+function convertFtoC(currentTemp) {
+    let celsiusTemp = (Number(currentTemp) * 9 / 5) + 32 
+    return celsiusTemp.toFixed(2)
+    }
+    
+    let currentTemp = 100
+    let celsiusTemp = convertFtoC(currentTemp)
+    
+    console.log(celsiusTemp)
