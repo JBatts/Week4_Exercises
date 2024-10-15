bc04195eb0dbbf3ee83f8ef7a63975feac3336fe
Jalen Battle <jalenbattle78@gmail.com>
2024-10-15 09:05:00 -0400

    first commit

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+    console.log("Jalen Battle")
+    console.log("Avengers: End-Game")
+    console.log("JuiceWrld")
+}
+
+function whyImHere() {
+    console.log("I joined Learning to Code to become a professional")
+}
+
+function favoritePlace() {
+    console.log("My favorite plave to visit is my girlfriend's apartment, because she always makes me happy!")
+}
+
+console.log(favoriteThings())
+console.log(whyImHere())
+console.log(favoritePlace())
