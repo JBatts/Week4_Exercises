e812632fea038b331fcf5ac5ee76bcfd0c855f99
Jalen Battle <jalenbattle78@gmail.com>
2024-10-16 11:02:24 -0400

    off by 1 website

---
+++ Dates/OffBy1Website/index.js
@@ -0,0 +1,17 @@
+// function onClick() {
+//     let date = document.getElementById("date")
+//     let dateObject = new Date(date)
+//     let prettyDate = dateObject.toLocaleDateString
+    
+//     let para = document.getElementById("para")
+//     para.innerText = convert(prettyDate)
+
+// }
+
+
+
+// button.addEventListener("click", onClick)
+
+function convert(date) {
+    return date
+}
