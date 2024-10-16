ffa9f7f3cfc492309b997b8f4a673c002bb19433
Jalen Battle <jalenbattle78@gmail.com>
2024-10-16 13:38:50 -0400

    walkthrough updated

--- Strings/parse.js
+++ Strings/parse.js
@@ -4,13 +4,19 @@
 // body of text is corpus
 
 corpus = "Now is the time..."
+console.log("LENGTH", corpus.length)
 
 // i = 0
 // console.log(corpus[i++])
 
 // i is for index
 // i++ is shorthand for i = i + 1
+// for (initialize/start; boolean expression tell us when to stop; what we want to happen)
 
 for(let i = 0 ; i < corpus.length; i++) {
 console.log(corpus[i]) // Repeated as long as the experssion is true
+}
+
+for(let i = 0 ; i < corpus.length; i++) {
+    console.log("INDEX:" + i, "LETTER:" + corpus[i], "CODE:" + corpus[i].charCodeAt(0))
 }
