059342ee5d09d4ec48954091b867131e0d4a24e6
Jalen Battle <jalenbattle78@gmail.com>
2024-10-16 13:24:57 -0400

    walkthrough for

---
+++ Strings/parse.js
@@ -0,0 +1,16 @@
+
+// PARSE - paring knife, parcel
+
+// body of text is corpus
+
+corpus = "Now is the time..."
+
+// i = 0
+// console.log(corpus[i++])
+
+// i is for index
+// i++ is shorthand for i = i + 1
+
+for(let i = 0 ; i < corpus.length; i++) {
+console.log(corpus[i]) // Repeated as long as the experssion is true
+}
