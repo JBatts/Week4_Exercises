26766914ca94cb6293eafc941b587ea2cc2927b8
Jalen Battle <jalenbattle78@gmail.com>
2024-10-16 15:42:06 -0400

    EOD walkthrough

---
+++ Strings/anon.js
@@ -0,0 +1,28 @@
+// What is a variable:
+// identifying label + a value
+// e.g. quantity = 123
+
+// What is a function?
+// A block of code we give a name to
+// A block of code we can call as many times as we like
+
+// A function is a value, that may or may not have identifier(function name)
+
+// Anonymous Functions
+
+a = 123
+b = a
+c = b
+console.log(c)
+
+function greet(firstName) { // This is a function (Tradition)
+    return "Howdy " + firstName
+}
+ console.log(greet("Juck the Overlord"));
+
+
+let greet2 = firstName => "howdy " + firstName // This is also a function (Treat a function as a value)
+
+hi = greet2 
+
+console.log(hi("Jucking"))
--- Strings/parse.js
+++ Strings/parse.js
@@ -1,6 +1,7 @@
 
 // PARSE - paring knife, parcel
-
+// substring(start, end) // No support for negative
+//substr(start, )
 // body of text is corpus
 
 corpus = "Now is the time..."
@@ -20,3 +21,8 @@ console.log(corpus[i]) // Repeated as long as the experssion is true
 for(let i = 0 ; i < corpus.length; i++) {
     console.log("INDEX:" + i, "LETTER:" + corpus[i], "CODE:" + corpus[i].charCodeAt(0))
 }
+
+words = corpus.split(" ")
+console.log(words)
+console.log("LENGTH (word count):", words.length)
+console.log("ENd")
